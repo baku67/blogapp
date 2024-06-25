b962377135f71262fe3df7d30d24b60dbe233151
baku67 <basile08@hotmail.fr>
2024-06-25 12:52:40 +0200

    reduce() categories from articles + filter categories onClick

--- src/index.js
+++ src/index.js
@@ -3,47 +3,102 @@ import { API_URL } from './utils';
 
 
 const articlesContainer = document.querySelector('.articles-container')
+const categoriesContainer = document.querySelector('.categories')
+let filter
+let articles
+
 
 // La fonction ne va pas au meme rythme car elle attend des infos
 const fecthArticles = async () => {
 
-    
     const response = await fetch(API_URL)
-    let articles = await response.json()
+    articles = await response.json()
 
     // Map marche que sur un Array, ducoup on met l'unique element dans un array
     if(!(articles instanceof Array)) {
         articles = [articles]
     }
 
-    createDOMArticles(articles)
+    createDOMArticles()
     console.log(articles)
+
+    createMenuCategories()
+}
+
+// reduce() des categories à partir des articles
+const createMenuCategories = () => {
+
+    const categories = articles.reduce((acc, currArticle) => {
+        if(acc[currArticle.category]) {
+            acc[currArticle.category]++
+        } else {
+            acc[currArticle.category] = 1
+        }
+        return acc  // car accolades
+    }, {})
+
+    // const categoriesArray = Object.keys(categories).map(category => [category, categories[category]]) //
+    const categoriesArray = Object.entries(categories)
+    console.log(categoriesArray)
+
+    displayMenuCategories(categoriesArray)
 }
 
 
-const createDOMArticles = (articles) => {
+
+const displayMenuCategories = (categoriesArray) => {
+    const liElements = categoriesArray.map( (categoryElement) => {
+        const li = document.createElement('li')
+        li.innerHTML = `${categoryElement[0]} <span>(${categoryElement[1]})</span>`
+        li.addEventListener('click', (e) => {
+            filter = categoryElement[0]
+            createDOMArticles()
+        })
+        return li
+    })
+
+    categoriesContainer.innerHTML = ""
+    categoriesContainer.append( ... liElements) // spread operator: explose les elements de l'array à l'intérieur
+}
+
+
+
+
+
+const createDOMArticles = () => {
 
     // map(): Prend un array et le transfomr en nouveau array 
     // Si accolades => return (sinon fleche joue le role de return) (!)
-    const articlesDOM = articles.map(article => {
+    const articlesDOM = articles
+    
+        .filter(article => {
+            if(filter) {
+                return article.category === filter
+            }
+            else {
+                return true // on filtre rien
+            }
+        })
 
-        const articleNode = document.createElement('div')
-        articleNode.classList.add('article')
-        articleNode.innerHTML = 
-        `
-            <img src=${article.image.startsWith('http') ? article.image : "assets/images/default_profil.png"} alt="photo">
-            <h2>${article.title}</h2>
-            <p class="article-author">${article.author} - <span>${
-                new Date(article.createdAt).toLocaleDateString('fr-FR')
-            }</span></p>
-            <p class="article-content">${article.content}</p>
-            <div class="article-actions">
-                <button class="btn btn-danger" data-id=${article._id}>Supprimer</button>
-                <button class="btn btn-primary" data-id=${article._id}>Editer</button>
-            </div>
-        `
-        return articleNode
-    })
+        .map(article => {
+
+            const articleNode = document.createElement('div')
+            articleNode.classList.add('article')
+            articleNode.innerHTML = 
+            `
+                <img src=${article.image.startsWith('http') ? article.image : "assets/images/default_profil.png"} alt="photo">
+                <h2>${article.title}</h2>
+                <p class="article-author">${article.author} - <span>${
+                    new Date(article.createdAt).toLocaleDateString('fr-FR')
+                }</span></p>
+                <p class="article-content">${article.content}</p>
+                <div class="article-actions">
+                    <button class="btn btn-danger" data-id=${article._id}>Supprimer</button>
+                    <button class="btn btn-primary" data-id=${article._id}>Editer</button>
+                </div>
+            `
+            return articleNode
+        })
 
     articlesContainer.innerHTML = '' // évite cumul
     articlesContainer.append(...articlesDOM) // On 'explose' articlesDOM dans appendChild
@@ -80,7 +135,6 @@ const createDOMArticles = (articles) => {
         })
     })
 }
-
 
 
 fecthArticles()
